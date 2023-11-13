8b1d63ee9dd95182d7fa10dbd7b60b8efd070575
Ryan <ryanyeojl@gmail.com>
2023-11-13 14:10:39 +0800

    improve on navbar design

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,17 @@ from application import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", index=True)
+
+@app.route('/about')
+def about():
+    return render_template("about.html", about=True)
+
+@app.route('/predict')
+def predict():
+    return render_template("predict.html", predict=True)
+
+@app.route('/history')
+def history():
+    return render_template("history.html", history=True)
