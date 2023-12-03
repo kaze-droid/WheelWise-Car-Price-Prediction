3cdd39aeeb1f6f1d8c13dc2a9ff8915cea4e5fdc
Ryan <ryanyeojl@gmail.com>
2023-12-04 00:31:41 +0800

    add test for front end routes

---
+++ tests/test_sites.py
@@ -0,0 +1,32 @@
+#1: Import the required modules
+from application.models import User
+import datetime as datetime
+import pytest
+from flask import json
+
+# Test all the front end routes
+@pytest.mark.parametrize("entrylist",
+        [
+         ["/"],
+         ["/about"],
+         ["/predict"],
+         ["/history"],
+         ["/register"],
+         ["/login"],
+         ["/profile"]
+        ])
+def test_front_end_routes(client, entrylist, capsys):
+    # Login user
+    data = {
+        'email': "testingAccount2@gmail.com",
+        'password': "abc123abc123",
+        'remember': False
+    }
+
+    response = client.post('/api/user', data=json.dumps(data), content_type='application/json')
+    assert response.status_code == 200
+
+    # Test the front end routes
+    response2 = client.get(entrylist[0])
+    assert response2.status_code == 200
+    assert response2.headers['Content-Type'] == 'text/html; charset=utf-8'
